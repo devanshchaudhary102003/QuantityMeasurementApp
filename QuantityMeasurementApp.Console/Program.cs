d3a0f0a03b97cd6f71698efafba48f35f5e5d05d
devanshchaudhary102003 <devanshchaudhary102003@gmail.com>
2026-03-15 22:49:50 +0530

    [Devansh Singh] Add.feature/UC16-Database-Integration-ADO.Net in our Quantity Measurement App

---
+++ QuantityMeasurementApp.Console/Program.cs
@@ -0,0 +1,11 @@
+﻿using QuantityMeasurementApp.Console.Menu;
+using QuantityMeasurementAppRepositoryLayer.Database;
+using QuantityMeasurementAppBusinessLayer.Interface;
+using QuantityMeasurementAppBusinessLayer.Service;
+using QuantityMeasurementAppRepositoryLayer.Interface;
+
+IQuantityMeasurementRepository repository = new QuantityMeasurementDatabase();
+IQuantityMeasurementService service = new QuantityMeasurementService(repository);
+Menu menu = new Menu(service);
+
+menu.Show();
